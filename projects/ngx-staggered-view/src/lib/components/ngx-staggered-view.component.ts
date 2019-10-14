92183f347e9876f959a53f8a155eb8f91624ee9b
Kushagra Saxena <kkss420@gmail.com>
2019-10-14 13:54:24 +0530

    added scss file for staggered view component

--- projects/ngx-staggered-view/src/lib/components/ngx-staggered-view.component.ts
+++ projects/ngx-staggered-view/src/lib/components/ngx-staggered-view.component.ts
@@ -4,7 +4,7 @@ import {NgxStaggeredViewDirective} from '../directives/ngx-staggered-view.direct
 @Component({
   selector: 'ngx-staggered-view',
   templateUrl: 'ngx-staggered-view.component.html',
-  styles: []
+  styleUrls: ['ngx-staggered-view.component.scss']
 })
 export class NgxStaggeredViewComponent implements OnInit {
   @ContentChild(NgxStaggeredViewDirective, {static: false, read: TemplateRef}) content: TemplateRef<any>;
--- projects/ngx-staggered-view/src/public-api.ts
+++ projects/ngx-staggered-view/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/ngx-staggered-view.service';
 export * from './lib/components/ngx-staggered-view.component';
+export * from './lib/directives/ngx-staggered-view.directive';
 export * from './lib/ngx-staggered-view.module';
