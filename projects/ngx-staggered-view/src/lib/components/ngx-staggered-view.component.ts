f2b028f50b76a8634d1966efeb650188da8b79fc
Kushagra Saxena <kkss420@gmail.com>
2019-10-14 12:46:46 +0530

    added directive and setup basic component working

--- projects/ngx-staggered-view/src/lib/components/ngx-staggered-view.component.ts
+++ projects/ngx-staggered-view/src/lib/components/ngx-staggered-view.component.ts
@@ -1,15 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ContentChild, OnInit, TemplateRef} from '@angular/core';
+import {NgxStaggeredViewDirective} from '../directives/ngx-staggered-view.directive';
 
 @Component({
   selector: 'ngx-staggered-view',
-  template: `
-      <p>
-          ngx-staggered-view works!
-      </p>
-  `,
+  templateUrl: 'ngx-staggered-view.component.html',
   styles: []
 })
 export class NgxStaggeredViewComponent implements OnInit {
+  @ContentChild(NgxStaggeredViewDirective, {static: false, read: TemplateRef}) content: TemplateRef<any>;
 
   constructor() { }
 
--- projects/ngx-staggered-view/src/lib/ngx-staggered-view.module.ts
+++ projects/ngx-staggered-view/src/lib/ngx-staggered-view.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {NgxStaggeredViewComponent} from './components/ngx-staggered-view.component';
+import {NgxStaggeredViewDirective} from './directives/ngx-staggered-view.directive';
+import {CommonModule} from '@angular/common';
 
 
 @NgModule({
-  declarations: [NgxStaggeredViewComponent],
-  imports: [],
-  exports: [NgxStaggeredViewComponent]
+  declarations: [NgxStaggeredViewComponent, NgxStaggeredViewDirective],
+  imports: [CommonModule],
+  exports: [NgxStaggeredViewComponent, NgxStaggeredViewDirective]
 })
 export class NgxStaggeredViewModule {}
